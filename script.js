5e03b48a95642326c6a1468c0cc927bbded2dd64
Claire Wanjiru <clairewanjiru.m@gmail.com>
2024-04-18 00:23:39 +0300

    added contents in each file

---
+++ script.js
@@ -0,0 +1,17 @@
+// declare variable and log value on console
+let myName ='claire';
+//  console.log(name);
+//  const age='17';
+//  console.log(age);
+ let hobby='swimming';
+ var car='toyota';
+
+    function logMyName() {
+        console.log(myName)
+    }
+     logMyName();
+function display() {
+    let userInputVar=document.getElementById('userInput').value;
+    console.log(userInputVar)
+    alert('Hello, ' + userInputVar);
+}
